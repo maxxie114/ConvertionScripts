6c6756a7108c74bf84a6cc68dbec173bbeda04e9
maxxie114 <maxxie114@gmail.com>
2019-11-24 05:08:10 -0800

    add bitwise and, bitwise or operation code

---
+++ bitwiseAnd.py
@@ -0,0 +1,18 @@
+import sys
+
+a = sys.argv[1]
+b = sys.argv[2]
+
+if not (len(a) == len(b)):
+  print("two binary numbers must have the same length")
+  exit()
+
+c = ""
+result = ""
+for i in range(len(a)):
+  c = int(a[i]) and int(b[i])
+  result += str(c)
+
+print("firstNumber:", a)
+print("secondNumber", b)
+print("finalResult:", result)
