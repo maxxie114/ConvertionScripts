1f67d5b570205f8ca7d724c415f420e2acb8861e
maxxie114 <maxxie114@gmail.com>
2020-01-12 21:26:34 -0800

    add a layer of base64 to xor encryption, to prevent unreable characters

--- xorencrypt.py
+++ xorencrypt.py
@@ -1,3 +1,6 @@
+# Add a layer of base64 encryption on top so that the encryption
+# result will all be readable characters
+import base64
 mode = input("select mode: \n a: encrypt \n b: decrypt\n")
 if mode == "a":
   modekey = "encrypt"
@@ -7,8 +10,12 @@ else:
   print("invalid mode")
   exit(0)
 in_a = input(f"input string to {modekey}:")
+if modekey == "decrypt":
+  in_a = base64.standard_b64decode(in_a).decode('utf-8')
 in_b = input("input encrypt char key:")
 result = ""
 for i in in_a:
   result += chr(ord(i) ^ ord(in_b))
-print("encrypted string:", result)
+if modekey == "encrypt":
+  result = base64.standard_b64encode(result.encode('utf-8'))
+print(f"{modekey}ed string: {result}")
