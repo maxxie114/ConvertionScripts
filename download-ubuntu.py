25e518db23d63d5010fb10f78fa5c4d01ccaa69a
maxxie114 <maxxie114@gmail.com>
2020-05-10 21:18:32 -0700

    add code to download both versions
    Add code to download both desktop and server version of the ISO file.

--- download-ubuntu.py
+++ download-ubuntu.py
@@ -11,3 +11,8 @@ print('Begin downloading ubuntu 20.04 desktop iso file')
 
 url = 'https://releases.ubuntu.com/20.04/ubuntu-20.04-desktop-amd64.iso?_ga=2.250071680.728293399.1589169307-865266594.1589169307'
 wget.download(url, 'ubuntu-20.04-desktop-amd64.iso')
+
+
+print("Begin donwloading ubuntu 20.04 server iso file")
+url2 = 'https://releases.ubuntu.com/20.04/ubuntu-20.04-live-server-amd64.iso?_ga=2.42847907.728293399.1589169307-865266594.1589169307'
+wget.download(url2, 'ubuntu-20.04-live-server-amd64.iso')
