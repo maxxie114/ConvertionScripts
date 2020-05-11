7e56e5633f53c1fba106233560e68dcfcc352191
maxxie114 <maxxie114@gmail.com>
2020-05-10 21:15:59 -0700

    Create download-ubuntu.py

---
+++ download-ubuntu.py
@@ -0,0 +1,13 @@
+# This program will download the ubuntu 20.04 iso file
+try:
+    import wget
+except:
+    import os
+    os.system("python3 -m pip install wget")
+    import wget
+
+# https://releases.ubuntu.com/20.04/ubuntu-20.04-desktop-amd64.iso?_ga=2.243191300.728293399.1589169307-865266594.1589169307
+print('Begin downloading ubuntu 20.04 desktop iso file')
+
+url = 'https://releases.ubuntu.com/20.04/ubuntu-20.04-desktop-amd64.iso?_ga=2.250071680.728293399.1589169307-865266594.1589169307'
+wget.download(url, 'ubuntu-20.04-desktop-amd64.iso')
